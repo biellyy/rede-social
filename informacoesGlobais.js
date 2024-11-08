b105f44c656ba2c64d51db332ee9b28d69cb315f
biellyy <105223864+biellyy@users.noreply.github.com>
2024-11-08 10:38:27 -0300

    aaba

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -3,7 +3,15 @@ Const url= 'https://github.com/guilhermeonrails/api'
 async function vizualizrInformaçoesGlobais() {
   const res = await fetch(url)
   const dados = await res.json()
-  console.log(dados);
+  const paragrafo = document.createElement('p')
+  paragrafo.classList.add('graficos-container__texto')
+  paragrafo.innerHTML=`Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média
+ ${dados.tempo_médio} horas conectadas.`
+ console.log(paragrafo);
+
+ const container = document.getElementById('graficos-container')
+ container.appendChild(paragrafo)
+ 
 }
 
 vizualizrInformaçoesGlobais()
