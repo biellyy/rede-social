ac6f73ec66117a04749ad16b8023549b6c53b1ab
biellyy <105223864+biellyy@users.noreply.github.com>
2024-11-22 08:08:25 -0300

    ava

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -4,14 +4,15 @@ async function vizualizrInformaçoesGlobais() {
   const res = await fetch(url)
   const dados = await res.json()
   const PessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
-  const PessoasNoMundo = (dados)
+  const PessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+  const horas = parseInt(dados.tempo_médio)
+  const minutos = (dados.tempo_médio - horas) * 100
 
 
   const paragrafo = document.createElement('p')
   paragrafo.classList.add('graficos-container__texto')
   paragrafo.innerHTML=`Você sabia que o mundo tem ${dados.total_pessoas_mundo}de pessoas e que aproximadamente <span> ${pessoas_conectadas} bilhões <span/> estão conectadas em alguma rede social e pass
-  am em média <span>
- ${dados.tempo_médio} </pan> horas conectadas.`
+  am em média <span>${horas} horas</pan> e  ${minutos} minutos conectadas.`
  
 
  const container = document.getElementById('graficos-container')
