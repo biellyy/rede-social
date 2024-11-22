71d1ac92f6d3714b786944aef9c528ab0e3f89fc
biellyy <105223864+biellyy@users.noreply.github.com>
2024-11-22 08:01:27 -0300

    aa

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -3,12 +3,16 @@ Const url= 'https://github.com/guilhermeonrails/api'
 async function vizualizrInformaçoesGlobais() {
   const res = await fetch(url)
   const dados = await res.json()
+  const PessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+  const PessoasNoMundo = (dados)
+
+
   const paragrafo = document.createElement('p')
   paragrafo.classList.add('graficos-container__texto')
-  paragrafo.innerHTML=`Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente <span> ${total_pessoas_conectadas} <span/> estão conectadas em alguma rede social e pass
+  paragrafo.innerHTML=`Você sabia que o mundo tem ${dados.total_pessoas_mundo}de pessoas e que aproximadamente <span> ${pessoas_conectadas} bilhões <span/> estão conectadas em alguma rede social e pass
   am em média <span>
  ${dados.tempo_médio} </pan> horas conectadas.`
- console.log(paragrafo);
+ 
 
  const container = document.getElementById('graficos-container')
  container.appendChild(paragrafo)
